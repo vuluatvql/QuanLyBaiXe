71f4b57310337c7aa05f6517a029359489a3bcc2
Leo <39667154+vuluatvql@users.noreply.github.com>
2018-11-17 21:29:07 +0700

    Add files via upload

---
+++ src/model/MyQuery.java
@@ -0,0 +1,32 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.mysql.jdbc.Statement;
+
+public class MyQuery {
+	
+	public static ResultSet excuteQuery(String sql) throws ClassNotFoundException, SQLException{
+		
+		Class.forName("com.mysql.jdbc.Driver");
+		String url = "jdbc:mysql://127.0.0.1/quanlybaixe";
+		String user = "root";
+		String password = "123456";
+		Connection con = DriverManager.getConnection(url, user, password);
+		System.out.println("Connection complete");
+		
+		PreparedStatement stmt = null;
+		stmt = con.prepareStatement(sql);
+		System.out.println("CreateStatement complete");
+		
+		ResultSet rs = stmt.executeQuery(sql);
+		System.out.println("ResultSet complete");
+		rs = stmt.executeQuery(sql);
+		
+		return rs;
+	}
+}
