cab5e6f0622a7157edb3e3d5a2f133b21bd697b6
Leo <39667154+vuluatvql@users.noreply.github.com>
2018-11-20 00:06:17 +0700

    Update quanlybaixe.sql

--- sql/quanlybaixe.sql
+++ sql/quanlybaixe.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 19, 2018 at 05:03 PM
+-- Generation Time: Nov 19, 2018 at 06:01 PM
 -- Server version: 10.1.36-MariaDB
 -- PHP Version: 7.2.11
 
