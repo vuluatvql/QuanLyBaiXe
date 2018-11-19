6fea0cc015a66f5e89e9e82dded01b5ea2b7337f
Leo <39667154+vuluatvql@users.noreply.github.com>
2018-11-19 23:01:59 +0700

    Add files via upload

--- src/model/MyQuery.java
+++ src/model/MyQuery.java
@@ -10,22 +10,49 @@ import com.mysql.jdbc.Statement;
 
 public class MyQuery {
 	
-	public static ResultSet excuteQuery(String sql) throws ClassNotFoundException, SQLException{
-		
-		Class.forName("com.mysql.jdbc.Driver");
-		String url = "jdbc:mysql://127.0.0.1/quanlybaixe";
-		String user = "root";
-		String password = "123456";
-		Connection con = DriverManager.getConnection(url, user, password);
-		System.out.println("Connection complete");
-		
-		PreparedStatement stmt = null;
-		stmt = con.prepareStatement(sql);
-		System.out.println("CreateStatement complete");
-		
-		ResultSet rs = stmt.executeQuery();
-		System.out.println("ResultSet complete");
-		
-		return rs;
+	String url = "jdbc:mysql://127.0.0.1/quanlybaixe";
+    String user = "root";
+    String pass = "123456";
+	
+	Connection conn;
+	
+	public MyQuery(){
+		try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(url, user, pass);
+            
+            if(conn != null){
+                System.out.println("Kết nối CSDL SQL Server thành công!");
+            }
+            
+		} catch (ClassNotFoundException ex) {
+            System.out.println(ex.toString());
+        } catch (SQLException ex) {
+            System.out.println(ex.toString());
+        }
 	}
+	
+	public ResultSet ExcuteQueryGetTable(String cauTruyVanSQL){
+        try {
+            java.sql.Statement stmt = conn.createStatement();           
+            ResultSet rs = stmt.executeQuery(cauTruyVanSQL);
+            System.out.println("Truy vấn thành công!");
+            return rs;
+        } catch (SQLException ex) {
+            System.out.println(ex.toString());
+        }
+            
+        return null;
+    }
+    //Thực thi INSERT, DELETE, UPDATE
+    public void ExcuteQueryUpdateDB(String cauTruyVanSQL){
+       
+        try {
+            java.sql.Statement stmt = conn.createStatement();
+            stmt.executeUpdate(cauTruyVanSQL);
+            System.out.println("Update thành công!");
+        } catch (SQLException ex) {
+            System.out.println(ex.toString());
+        }
+    }
 }
